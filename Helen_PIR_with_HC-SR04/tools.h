f29305adc5b66372546beafc52f88864e39fe8ab
Samuel-de-Oliveira <samwolfg13@gmail.com>
2022-11-07 15:28:40 -0300

    Add the \´PIR with HC-SR04\´ code (Isn't done yet)

---
+++ Helen_PIR_with_HC-SR04/tools.h
@@ -0,0 +1,30 @@
+/*   -*- Tools -*-
+ *  
+ * Code of all tools here.
+ *
+ * Version: 1.0
+ * 
+ */
+
+
+// ----- *X* ----- //
+
+
+// Show the distance
+void showDistance( Ultrasonic Sensor ) {
+
+  /*   -*- Show the Distance (In centimetres and inches) -*-
+  /* * ------------------------ * */
+  Serial.println("||---------------------||");
+  Serial.print(Sensor.read(CM));
+  Serial.print(" Cm, ");
+  Serial.print(Sensor.read(INC));
+  Serial.println(" Inc.");
+  Serial.println("||---------------------||");
+  /* * ------------------------ * */
+
+}
+
+void isHot() {
+
+}
